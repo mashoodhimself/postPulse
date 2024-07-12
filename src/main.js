a7124ff1954e3e1ed9ef2a931d2f979b7fc1aa4a
Mashhood <mashoodahmad50@gmail.com>
2024-07-12 10:30:10 +0500

    Bootstrap template integrated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import './assets/main.css'
+// import './"./assets/main.css'
+import "./assets/vendor/bootstrap/css/bootstrap.min.css"
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css"
+import "./assets/vendor/boxicons/css/boxicons.min.css"
+import "./assets/vendor/quill/quill.snow.css"
+import "./assets/vendor/quill/quill.bubble.css"
+import "./assets/vendor/remixicon/remixicon.css"
+import "./assets/vendor/simple-datatables/style.css"
+import "./assets/css/style.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
